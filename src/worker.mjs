9b8baeb15058bbe037138046e7d890fd89aee247
Dalibor <mail@daliborgogic.com>
2021-10-23 09:43:37 +0200

    feat: test file

--- src/worker.mjs
+++ src/worker.mjs
@@ -1,7 +1,9 @@
+import { txt } from './txt.mjs'
+
 export default {
   async fetch(request, env) {
     try {
-      return await new Response('Wohoo!')
+      return await new Response(txt)
     } catch (e) {
       return new Response(e.message)
     }
